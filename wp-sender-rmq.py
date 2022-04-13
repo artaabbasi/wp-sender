3262fcd76e044d3716fab72a1938e1109554eedb
artaNova <arta1382@gmail.com>
2022-04-13 13:02:39 +0430

    fix bugs

--- wp-sender-rmq.py
+++ wp-sender-rmq.py
@@ -52,14 +52,14 @@ from config.settings import BASE_DIR
 if system().lower() == "linux":
     driver_exe = "chromium/linux/chromedriver"
 elif system().lower() == "windows":
-    driver_exe = "chromium/windows/chromedriver.exe"
+    driver_exe = r"chromium\windows\chromedriver.exe"
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 global driver
-driver = webdriver.Chrome(executable_path="/home/arta/Downloads/chromedriver_linux64/chromedriver")
+driver = webdriver.Chrome(executable_path=driver_exe)
 driver.get(f"https://web.whatsapp.com/")
 
 def callback(ch, method, properties, body):
@@ -96,22 +96,24 @@ def callback(ch, method, properties, body):
     inp_xpath_search_box = WebDriverWait(driver,50).until(lambda driver: driver.find_element(by=By.XPATH, value=inp_xpath_search))
     inp_xpath_search_box.click()
     inp_xpath_search_box.send_keys('Wp test')
-    selected_group = driver.find_element(by=By.XPATH, value="//span[@title='Wp test']")
+    selected_group = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH, value="//span[@title='Wp test']"))
     selected_group.click()
     inp_xpath = '//div[@title="Type a message"]'
     input_box = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH, value=inp_xpath))
     time.sleep(random.randint(5, 10))
     contact = f"http://wa.me/{phone}"
-
-    input_box.send_keys(contact+Keys.ENTER)
-    time.sleep(random.randint(7, 11))
-    message_link = driver.find_element(by=By.XPATH , value = f'//a[@href="{contact}"]')
+    try:
+        message_link = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH , value = f'//a[@href="{contact}"]'))
+    except:
+        input_box.send_keys(contact+Keys.ENTER)
+        time.sleep(random.randint(7, 11))
+        message_link = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH , value = f'//a[@href="{contact}"]'))
     message_link.click()
     time.sleep(random.randint(3, 9))
-    if media != None:
+    if media is not None:
         from io import BytesIO
 
-        import pyperclip as pc
+        import win32clipboard
         from PIL import Image
 
         image = Image.open(media)
@@ -119,10 +121,19 @@ def callback(ch, method, properties, body):
         image.convert("RGB").save(output, "BMP")
         data = output.getvalue()[14:]
         output.close()
-        pc.copy(data)
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+        win32clipboard.CloseClipboard()
+
         input_box_after_link = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH, value=inp_xpath))
-        input_box_after_link.send_keys(text + Keys.CONTROL + "v")
-        
+        input_box_after_link.send_keys(text)
+        input_box_after_link.send_keys(Keys.CONTROL + "v")
+        inp_xpath = '//div[@role="button"][@aria-label="Send"]'
+        input_box_after_link_image = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH, value=inp_xpath))
+        time.sleep(2)
+        input_box_after_link_image.click()
+                
     else:
         input_box_after_link = WebDriverWait(driver,5).until(lambda driver: driver.find_element(by=By.XPATH, value=inp_xpath))
         input_box_after_link.send_keys(text + Keys.ENTER)
