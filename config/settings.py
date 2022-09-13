8c3600a9346a00f08df6a16a2561874fc94658a2
arta <arta1382@gmail.com>
2022-09-13 14:51:16 +0430

    csrf bug

--- config/settings.py
+++ config/settings.py
@@ -45,13 +45,13 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['*']
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
