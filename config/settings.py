b021ed4506ca7636ac644002e99cf1616d66ad87
arta <arta1382@gmail.com>
2022-09-13 14:31:25 +0430

    csrf bug

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication'
     ),
 }
